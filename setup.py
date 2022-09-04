6e6fca7885653b592c5ec1e1ac8e08f5759b25b3
Alvaro Avila <almiavicas@gmail.com>
2022-09-03 21:04:26 -0400

    modified folder name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 setup(
     name='as-scraper',
-    version='1.0.5',
+    version='1.0.6',
     description='Python library for scraping inside Airflow.',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
